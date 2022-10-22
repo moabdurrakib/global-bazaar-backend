a9ec732958fa2adf4464230ad7796909bdb3b208
mabdurrakib <mabdurrakib.dev@gmail.com>
2022-10-22 17:21:03 +0600

    Laravel Model,Migration and Factory Create part-3

---
+++ backend-server/database/migrations/2022_10_22_104541_create_seller_file_managers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seller_file_managers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('seller_id');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seller_file_managers');
+    }
+};
---
+++ backend-server/database/migrations/2022_10_22_104745_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('order_number');
+            $table->text('shipping_address');
+            $table->string('payment_type')->default('cash');
+            $table->float('subtotal',8,2);
+            $table->float('discount',8,2);
+            $table->float('total',8,2);
+            $table->enum('status',['active','inactive'])->default('inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
