1a5cdad00ce826aed3571b1ea1318694aef1cb68
mabdurrakib <mabdurrakib.dev@gmail.com>
2022-10-28 23:07:25 +0600

    #13 Vue3 - vue-router setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import './template.js'
-import router from './router/index'
+
+import router from './router'
 import App from './App.vue'
 
-const app =createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App)
+app.use(router);
+app.mount('#app');
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,16 @@
 import {createRouter ,createWebHistory} from 'vue-router';
-import index from '../views/pages/index.vue'
+import Index from '../views/pages/Index.vue'
 
 const routes = [
     {
         path: "/",
-        commponent: index
+        commponent: Index
     }
 ]
 
 const router =createRouter({
-    history:createWebHistory(import.meta.env.BASE_URL),
-    routes
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
 })
 
 export default router;
