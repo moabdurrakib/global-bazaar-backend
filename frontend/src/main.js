53f1108f453a36a5fb4dc207683e42f0ed69d453
mabdurrakib <mabdurrakib.dev@gmail.com>
2022-11-03 16:09:32 +0600

    #20 Vue3 - Dynamically Change page title - using vue router guard

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import './template.js'
 
-import {router} from "./router";
+import router from "./router";
 import App from "./App.vue";
 
 createApp(App).use(router).mount("#app");
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,27 +7,44 @@ const routes = [
     {
         path: "/",
         commponent: Index,
-        name: 'index.page'
+        name: 'index.page',
+        meta: {title:"home"}
     },
     
     { path: '/:pathMatch(.*)*',
      name: 'NotFound', 
-     component: NotFound 
+     component: NotFound ,
+     meta: {title:"Not Found"}
     },
 
     // auth
     { path: '/auth/login',
     name: 'user.login', 
-    component: UserLogin 
+    component: UserLogin ,
+    meta: {title:"User Login"}
    },
     { path: '/auth/register',
     name: 'user.register', 
-    component: UserRegister 
+    component: UserRegister ,
+    meta: {title:"User Register"}
    },
 
 ]
 
-export const router = createRouter({
+
+
+
+const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 })
+
+const DEFAULT_TITLE = '404'
+
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title || DEFAULT_TITLE
+     next()
+  })
+
+
+export default router;
