6371a0a5a5f51f9d4a7bf74f15395dff99fd6f70
mabdurrakib <mabdurrakib.dev@gmail.com>
2022-11-06 19:28:16 +0600

    Vue3 & Pinia - Display Server Side Validation error fix

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -1,17 +1,23 @@
 import { defineStore } from "pinia"
 import axios from 'axios'
-export const storeAuth = defineStore('auth', {
+export const useAuth = defineStore('auth', {
+    state: ()=>({errors:{} }),
     actions: {
         async login(formData) {
             try {
-                let res = await axios.post(import.meta.env.VITE_API_URL + "/api/v1/user/login", formData)
+                let res = await axios.post(
+                    import.meta.env.VITE_API_URL + "/api/v1/user/login",
+                     formData)
 
                 if (res.status === 200) {
                     console.log(res.data);
                 }
+
             }
             catch (error) {
-                console.log(error);
+                if (error.response.data){
+                    this.errors= error.response.data.errors;
+                }
             }
 
         }
--- frontend/src/views/auth/login.vue
+++ frontend/src/views/auth/login.vue
@@ -1,96 +1,113 @@
 <script setup>
-import {reactive,ref} from 'vue'
-import { storeAuth } from '@/stores/auth';
+import { reactive, ref } from "@vue/reactivity";
+import { useAuth } from "@/stores/auth";
+import { storeToRefs } from "pinia";
 
-const auth = storeAuth();
+const auth = useAuth();
+const { errors } = storeToRefs(auth);
 
 const loginForm = reactive({
-  phone:"",
-  password:""
-})
+  phone: "",
+  password: "",
+});
 
-const loginSubmit =async() =>{
-  await auth.login(loginForm)
-}
+const loginSubmit = async () => {
+  await auth.login(loginForm);
+};
 
-const showPassword= ref(false)
+const showPassword = ref(false);
 
-const toggleShow=()=>{
-  showPassword.value = !showPassword.value
-}
+const toggleShow = () => {
+  showPassword.value = !showPassword.value;
+};
 </script>
 <template>
-    <div>
-        <section class="user-form-part">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-12 col-sm-10 col-md-12 col-lg-12 col-xl-6">
-              <div class="user-form-card">
-                <div class="user-form-title">
-                  <h2>Customer Login</h2>
-                  <p>Use your credentials to access</p>
-                </div>
-                <div class="user-form-group" id="axiosForm">
-                  <form class="user-form" @submit.prevent="loginSubmit">
-                    <!--v-if-->
-                    <div class="form-group">
-                      <input
-                        :type="text"
-                        class="form-control"
-                        placeholder="phone no"
-                        v-model="loginForm.phone"
-                      /><!--v-if-->
-                    </div>
-                    <div class="form-group">
-                      <input
+  <div>
+    <section class="user-form-part">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-sm-10 col-md-12 col-lg-12 col-xl-6">
+            <div class="user-form-card">
+              <div class="user-form-title">
+                <h2>Customer Login</h2>
+                <p>Use your credentials to access</p>
+              </div>
+              <div class="user-form-group" id="axiosForm">
+                <form class="user-form" @submit.prevent="loginSubmit">
+                  <!--v-if-->
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="phone no"
+                      v-model="loginForm.phone"
+                      :class="{'is-invalid':errors.phone}"
+                    /><!--v-if-->
+                    <span class="text-danger" v-if="errors.phone">{{
+                      errors.phone[0]
+                    }}</span>
+                  </div>
+
+                  <div class="form-group">
+                    <input
                       :type="showPassword ? 'text' : 'password'"
-                        class="form-control"
-                        placeholder="password"
-                        v-model="loginForm.password"
-                      /><span @click="toggleShow" class="view-password"
-                        ><i class="fas text-success" :class="{
+                      class="form-control"
+                      placeholder="password"
+                      v-model="loginForm.password"
+                      :class="{'is-invalid':errors.password}"
+                    />
+                    <span class="text-danger" v-if="errors.password">{{
+                      errors.password[0]
+                    }}</span>
+                    <span @click="toggleShow" class="view-password"
+                      ><i
+                        class="fas text-success"
+                        :class="{
                           'fa-eye-slash': showPassword,
                           'fa-eye': !showPassword,
-                        }"></i></span
-                      ><!--v-if-->
-                    </div>
-                    <div class="form-check mb-3">
-                      <input
-                        class="form-check-input"
-                        type="checkbox"
-                        id="check"
-                        value=""
-                      /><label class="form-check-label" for="check"
-                        >Remember Me</label
+                        }"
+                      ></i></span
+                    ><!--v-if-->
+                  </div>
+                  <div class="form-check mb-3">
+                    <input
+                      class="form-check-input"
+                      type="checkbox"
+                      id="check"
+                      value=""
+                    /><label class="form-check-label" for="check"
+                      >Remember Me</label
+                    >
+                  </div>
+                  <div class="form-button">
+                    <button type="submit">login</button>
+                    <p>
+                      Forgot your password?<a
+                        href="reset-password.html"
+                        class=""
+                        >reset here</a
                       >
-                    </div>
-                    <div class="form-button">
-                      <button type="submit">login</button>
-                      <p>
-                        Forgot your password?<a
-                          href="reset-password.html"
-                          class=""
-                          >reset here</a
-                        >
-                      </p>
-                    </div>
-                  </form>
-                </div>
+                    </p>
+                  </div>
+                </form>
               </div>
-              <div class="user-form-remind">
-                <p>
-                  Don't have any account? <router-link :to="{name:'user.register'}">register here</router-link>
-                </p>
-              </div>
-              <div class="user-form-footer"></div>
             </div>
+            <div class="user-form-remind">
+              <p>
+                Don't have any account?
+                <router-link :to="{ name: 'user.register' }"
+                  >register here</router-link
+                >
+              </p>
+            </div>
+            <div class="user-form-footer"></div>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style>
-
 @import "@/assets/css/user-auth.css";
 </style>
